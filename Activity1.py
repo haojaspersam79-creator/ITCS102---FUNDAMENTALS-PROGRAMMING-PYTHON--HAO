e511f82633b630522b3f8a3dc5f387d829fd0f8c
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:42:53 +0800

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+Hi, I am Jasper Sam M. Hao, student from BSIT First Year section 1A
+
+This is my First file in Repository.
+
+I am 18 yrs old, from Brgy. 3 Lucena City
+
+
+I am a Working Student, I love Eating and Playing games
