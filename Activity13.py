1b0180d3e174648d84e2bc3f30cbf44047d0d75e
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:53:10 +0800

    Add files via Upload
    Implement a for loop to input 10 numbers and print the last input.

---
+++ Activity13.py
@@ -0,0 +1,9 @@
+#using for loop, ask user to input 10 numbers
+#after input, get the summation all the numbers 
+
+for P in range (1, 11):
+    num = 0
+    number = eval(input("Enter a number "))
+    num += number
+    print("Siomai Rice", num)
+print(num)
