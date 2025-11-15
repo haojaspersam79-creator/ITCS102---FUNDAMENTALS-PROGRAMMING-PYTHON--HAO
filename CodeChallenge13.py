598473e8add2422112cdbda72a27c2402e67e150
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 13:03:36 +0800

    Add files via Upload

---
+++ CodeChallenge13.py
@@ -0,0 +1,51 @@
+#Christmas Tree
+for j in range(2,6,1):
+    for p in range(0,7):
+        print(" ",end=" ")
+    for a in range(6,j,-1):
+        print(" ",end=" ")
+    for s in range(3,j):
+        print("*",end=" ")
+    for a in range(2,j,1):
+        print("*",end=" ")
+    print()    
+for m in range(1,3,1):
+    for b in range(0,7):
+        print(" ",end=" ")
+    for a in range(0 - 1,m,1):
+        print(" ",end=" ")
+    for l in range(2,m,-1):
+        print("*",end=" ")
+    for o in range(3,m,-1):
+        print("*",end=" ")
+    print()    
+for v in range(2,9,1):
+    for e in range(12,v,-1):
+        print(" ",end=" ")
+    for s in range(2,v,1):
+        print("*",end=" ")
+    for j in range(1,v):
+        print("*",end=" ")
+    print()
+for a in range(2,10,1):
+    for s in range(12,a,-1):
+        print(" ",end=" ")
+    for p in range(1,a):
+        print("*",end=" ")
+    for e in range(2,a):
+        print("*",end=" ")
+    print()
+for r in range(2,13,1):
+    for s in range(12,r,-1):
+        print(" ",end=" ")
+    for a in range(1,r):
+        print("*",end=" ")
+    for m in range(2,r):
+        print("*",end=" ")
+    print()
+for h in range(1,7):
+    for a in range(1,9):
+        print(" ",end=" ")
+    for o in range(1,9):
+        print("",end="*")
+    print()
