0322b52721f14da0423af8f68813e9f0241b7c7c
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 12:00:18 +0800

    Add files via Upload

---
+++ Activity25_1.py
@@ -0,0 +1,8 @@
+def activity1():
+    name = input("What is you're name: ")
+def activity2():
+    age = eval(input("How old are you: "))
+def activity3():
+    Hob = input("What is you're Hobby: ")
+def activity4():
+    gen = input("Male/Female -> ")
