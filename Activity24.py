3532f168b940241b12ad33f0eafb4119ed58cd9b
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:59:24 +0800

    Add files via Upload

---
+++ Activity24.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print(f"Hi {name}, Happy Friday! ")
+
+greet("Sam")
+greet("Friends")
+
+def summation(j):
+    sum = 0
+    for j in range(j, 0, -1):
+        sum += j
+    print(f"The sum of {j} is {sum}")
+
+summation(1)
+summation(2)
