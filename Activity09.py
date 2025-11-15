7ab7489b8d32f4a24f3540bb415fb9d2976677fd
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:49:16 +0800

    Add files via Upload

---
+++ Activity09.py
@@ -0,0 +1,7 @@
+username = 'JasperSam'
+password = 'CuteniSelf'
+
+#print(username == 'JasperSam')
+#print((username == 'JasperSam') and (password == 'CuteniSelf'))
+#print((username == 'JasperSam') or (password == 'CuteniSelf'))
+print(not((username == 'JasperSam') or (password == 'CuteniSelf')))
