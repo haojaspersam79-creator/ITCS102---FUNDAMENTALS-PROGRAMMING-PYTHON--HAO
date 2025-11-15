74a874e4d06e68724eb40d5263510f28679b8de4
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 12:24:06 +0800

    Add files via Upload

---
+++ CodeChallenge03.py
@@ -0,0 +1,16 @@
+#username
+#password
+#login
+#access granted
+
+username = 'jsam'
+password = 'cuteako'
+
+userinput = input("username --> ")
+passinput = input("type your password --> ")
+
+if userinput == username and passinput == password:
+	print("Access Granted!")
+
+else:
+	print("Access Denied")
