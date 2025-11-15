97bc3433d052c54f05969780be51b68c89c8abe1
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:56:30 +0800

    Add files via Upload

---
+++ Activity20.py
@@ -0,0 +1,21 @@
+#for j in range(1,11,1):
+    #for p in range(1,j,1):
+       #print(" ", end= ' ')
+    #for a in range("1",10,-1):
+        #print("*",end = " ")
+    #print()
+
+for j in range(1,11,1):
+    for p in range(1,j,1):
+       print(" ", end= ' ')
+    for a in range("1",10,-1):
+        print("*",end = " ")
+    print()
+
+for j in range(1,11,1):
+    for p in range(1,j,1):
+       print(" ", end= ' ')
+    for a in range("1",10,-1):
+        print("*",end = " ")
+    print()
+
