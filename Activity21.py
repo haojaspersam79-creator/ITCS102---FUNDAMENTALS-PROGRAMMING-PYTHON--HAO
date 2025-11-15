39aceff3ba43454ae5def3a51a6a58e7e8f0f728
haojaspersam79-creator <haojaspersam79@gmail.com>
2025-11-15 11:58:05 +0800

    Add files via Upload

---
+++ Activity21.py
@@ -0,0 +1,18 @@
+#while loop
+
+me = True
+
+while me == True:
+    moon = input("(Yes or No): ")
+
+    if moon.lower() == "yes":
+        print("COntinue")
+        continue
+    elif moon.lower() == "no":
+        print("Stop")
+        break
+    else:
+        print("Error input")
+        continue
+Sent
+Write to Only For Myself
